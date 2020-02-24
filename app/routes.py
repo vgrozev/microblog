bfeaeaa0d6a70ec181293897172074433d8e0e1d
Vlado <vgrozev@gmail.com>
2020-02-24 02:24:26 -0500

    login forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -17,10 +20,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-# this import should be at the top under '...app import...'
-from app.forms import LoginForm
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = loginForm()
-	return render_template('login.html', title='Sign In', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
